cd390d9da0c9df263dc0344aadf410ac60e96c9b
dev <dev@kierans-portfolio.attlocal.net>
2020-09-02 16:50:06 -0500

    added apod component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+// import looks for a given class from a given file location
 import { Component } from '@angular/core';
 
+// All components have a component decorator. A component decorator is where we create the relationship between components, styles, and templates.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// All Angular components export a class. This class contains the business logic that powers a given component. This logic affects the *.component.html file that is defined by templateUrl in the component decorator.
 export class AppComponent {
-  title = 'ng-apod';
+  title = 'NASA\'s Astronomy Picture of the Day';
 }
